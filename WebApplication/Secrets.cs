ac9d371bca3f162950223020d6bcd9b9c3244588
Zdeněk Pašek <39481226+TenZdenda@users.noreply.github.com>
2021-12-09 12:39:25 +0100

    Update Secrets.cs

--- WebApplication/Secrets.cs
+++ WebApplication/Secrets.cs
@@ -2,6 +2,6 @@ namespace WebApplication
 {
     public class Secrets
     {
-        public static readonly string ConnectionString = "Server=localhost;Database=school_project;Uid=sa;Pwd=someThingComplicated1234;";
+        public static readonly string ConnectionString = "Server=tcp:mold-backend-school-project.database.windows.net,1433;Initial Catalog=mold_backend;Persist Security Info=False;User ID=zdenek;Password=xZGAD.NEaACB7_HAp!dQnUNJdKBduBZ9rH-vVnDE;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
